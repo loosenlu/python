aec518d997828f04f988e398f7be27482c2aa132
Luli <loosenlu@gmail.com>
2016-12-06 11:50:57 +0800

    12.6

--- PyLuaTblParser.py
+++ PyLuaTblParser.py
@@ -3,13 +3,13 @@
 
 def extrat_dict_item(lua_table, start_index):
     '''
-        return  dict_item(string format)
+        return  a dict_item range[start_index, end_index)
     '''
     cur_index = start_index
     first_comma_index = lua_table.find(',', start_index)
     first_braket_index = lua_table.find('{', start_index)
     if first_comma_index < first_braket_index | first_comma_index == -1:
-        return first_comma_index
+        return start_index, first_comma_index
 
 
     left_braket_num = 0
@@ -22,8 +22,9 @@ def extrat_dict_item(lua_table, start_index):
             # It means the '{' is marched
             if left_braket_num == 0:
                 break
+        cur_index += 1
 
-    return cur_index
+    return first_braket_index + 1, cur_index
 
 
 
@@ -35,22 +36,34 @@ def str_resolve(lua_table):
     while cur_index < len(lua_table):
 
         if lua_table[cur_index] == '=':
+
             is_dict = True
             dict_item_key = lua_table[pre_index:cur_index]
-            next_index = extrat_dict_item(lua_table, cur_index + 1)
-            dict_item_value = lua_table[cur_index + 1: next_index]
-            component_container.append({dict_item_key:dict_item_value})
-            cur_index = next_index + 1
+
+            item_start, item_end = extrat_dict_item(lua_table, cur_index + 1)
+            dict_item_value = lua_table[item_start: item_end]
+            component_container.append({dict_item_key.strip():dict_item_value.strip()})
+            cur_index = item_end + 1
+            pre_index = cur_index
             continue
         elif lua_table[cur_index] == ',':
-            
-            
-        
+
+            if pre_index == cur_index:
+                cur_index += 1
+                pre_index = cur_index
+                continue
+            list_item = lua_table[pre_index:cur_index]
+            component_container.append(list_item.strip())
+            pre_index = cur_index + 1
+            cur_index += 1
+            continue
+        cur_index += 1
+    return component_container
 
 
 
 lua_table = '{array = {65,23,5,},\
             dict = {mixed = {43,54.33,false,9,string = "value",},\
                 array = {3,6,4,},string = "value",},}'
-l = str_resolve
-            
+l = str_resolve(lua_table)
+print l
